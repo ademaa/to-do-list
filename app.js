0afdc27ba17b6079d3e59ee30ee39224d3de2fe1
omar adam <omar_adam1000@hotmail.com>
2020-11-15 14:58:14 +0200

    syntax update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
